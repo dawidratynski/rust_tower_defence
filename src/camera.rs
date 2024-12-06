0cdbc8f4b6223b7cdf9b85b811df435c895f7014
Dawid Ratyński <dawidratynski@gmail.com>
2024-12-06 22:31:35 +0100

    Add camera rotation and zoom

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,11 @@
 use crate::*;
 
-const CAMERA_SPEED: f32 = 200.0;
+const CAMERA_SPEED: f32 = 500.0;
+const CAMERA_ROTATION_SPEED: f32 = 1.5;
+const CAMERA_SCALE_SPEED: f32 = 1.0;
+
+const CAMERA_MAX_SCALE: f32 = 3.0;
+const CAMERA_MIN_SCALE: f32 = 0.2;
 
 pub struct CameraPlugin;
 
@@ -21,21 +26,44 @@ fn spawn_camera(mut commands: Commands) {
 
 fn camera_control(
     keys: Res<ButtonInput<KeyCode>>,
-    mut camera_query: Query<&mut Transform, With<Camera2d>>,
+    mut camera_query: Query<(&mut Transform, &mut OrthographicProjection), With<Camera2d>>,
     time: Res<Time>,
 ) {
-    let mut camera = camera_query.single_mut();
+    let (mut camera_transform, mut camera_projection) = camera_query.single_mut();
+
+    let local_right = camera_transform.right();
+    let local_up = camera_transform.up();
 
     if keys.any_pressed([KeyCode::KeyW, KeyCode::ArrowUp]) {
-        camera.translation.y += CAMERA_SPEED * time.delta_secs();
+        camera_transform.translation += local_up * CAMERA_SPEED * time.delta_secs();
     }
     if keys.any_pressed([KeyCode::KeyA, KeyCode::ArrowLeft]) {
-        camera.translation.x -= CAMERA_SPEED * time.delta_secs();
+        camera_transform.translation -= local_right * CAMERA_SPEED * time.delta_secs();
     }
     if keys.any_pressed([KeyCode::KeyS, KeyCode::ArrowDown]) {
-        camera.translation.y -= CAMERA_SPEED * time.delta_secs();
+        camera_transform.translation -= local_up * CAMERA_SPEED * time.delta_secs();
     }
     if keys.any_pressed([KeyCode::KeyD, KeyCode::ArrowRight]) {
-        camera.translation.x += CAMERA_SPEED * time.delta_secs();
+        camera_transform.translation += local_right * CAMERA_SPEED * time.delta_secs();
+    }
+
+    if keys.any_pressed([KeyCode::KeyQ]) {
+        camera_transform.rotate_z(CAMERA_ROTATION_SPEED * time.delta_secs());
+    }
+    if keys.any_pressed([KeyCode::KeyE]) {
+        camera_transform.rotate_z(-CAMERA_ROTATION_SPEED * time.delta_secs());
+    }
+
+    if keys.any_pressed([KeyCode::KeyZ]) {
+        camera_projection.scale += CAMERA_SCALE_SPEED * time.delta_secs();
+        if camera_projection.scale > CAMERA_MAX_SCALE {
+            camera_projection.scale = CAMERA_MAX_SCALE;
+        }
+    }
+    if keys.any_pressed([KeyCode::KeyX]) {
+        camera_projection.scale -= CAMERA_SCALE_SPEED * time.delta_secs();
+        if camera_projection.scale < CAMERA_MIN_SCALE {
+            camera_projection.scale = CAMERA_MIN_SCALE;
+        }
     }
 }
