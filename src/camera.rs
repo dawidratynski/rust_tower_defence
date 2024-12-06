baed0035550b67964b31667e180768efd2bc0e0f
Dawid Ratyński <dawidratynski@gmail.com>
2024-12-06 22:15:47 +0100

    Add camera controls

--- src/camera.rs
+++ src/camera.rs
@@ -1,12 +1,13 @@
 use crate::*;
 
+const CAMERA_SPEED: f32 = 200.0;
+
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app
-        .add_systems(Startup, spawn_camera)
-        .add_systems(Update, camera_control);
+        app.add_systems(Startup, spawn_camera)
+            .add_systems(Update, camera_control);
     }
 }
 
@@ -18,7 +19,23 @@ fn spawn_camera(mut commands: Commands) {
     ));
 }
 
+fn camera_control(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut camera_query: Query<&mut Transform, With<Camera2d>>,
+    time: Res<Time>,
+) {
+    let mut camera = camera_query.single_mut();
 
-fn camera_control(){
-
+    if keys.any_pressed([KeyCode::KeyW, KeyCode::ArrowUp]) {
+        camera.translation.y += CAMERA_SPEED * time.delta_secs();
+    }
+    if keys.any_pressed([KeyCode::KeyA, KeyCode::ArrowLeft]) {
+        camera.translation.x -= CAMERA_SPEED * time.delta_secs();
+    }
+    if keys.any_pressed([KeyCode::KeyS, KeyCode::ArrowDown]) {
+        camera.translation.y -= CAMERA_SPEED * time.delta_secs();
+    }
+    if keys.any_pressed([KeyCode::KeyD, KeyCode::ArrowRight]) {
+        camera.translation.x += CAMERA_SPEED * time.delta_secs();
+    }
 }
