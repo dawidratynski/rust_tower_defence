a477a66303657f5fea1d925a1c06f0cc70a14378
Dawid Ratyński <dawidratynski@gmail.com>
2025-01-06 14:46:16 +0100

    Implement pausing

--- src/game_time.rs
+++ src/game_time.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 use std::time::Duration;
 
 const TIME_SPEEDUP_KEY: KeyCode = KeyCode::KeyC;
+const PAUSE_KEY: KeyCode = KeyCode::KeyP;
 const TIME_SPEEDUP_SCALE: f32 = 2.0;
 
 pub struct GameTimePlugin;
@@ -12,9 +13,12 @@ impl Plugin for GameTimePlugin {
         app.insert_resource(GameTime {
             scale: 1.0,
             delta_secs: 0.0,
+            paused: false,
         })
+        .add_systems(Startup, setup_pause_ui)
         .add_systems(PreUpdate, update_game_time)
-        .add_systems(PreUpdate, time_flow_controls);
+        .add_systems(PreUpdate, time_flow_controls)
+        .add_systems(PreUpdate, handle_pause);
     }
 }
 
@@ -22,11 +26,16 @@ impl Plugin for GameTimePlugin {
 pub struct GameTime {
     pub scale: f32,
     delta_secs: f32,
+    paused: bool,
 }
 
 impl GameTime {
     pub fn delta_secs(&self) -> f32 {
-        self.delta_secs * self.scale
+        if self.paused {
+            0.0
+        } else {
+            self.delta_secs * self.scale
+        }
     }
 
     pub fn delta(&self) -> Duration {
@@ -44,4 +53,65 @@ fn time_flow_controls(keys: Res<ButtonInput<KeyCode>>, mut game_time: ResMut<Gam
     } else {
         game_time.scale = 1.0;
     }
+}
+
+#[derive(Component)]
+struct PauseOverlay;
+
+fn handle_pause(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut game_time: ResMut<GameTime>,
+    mut query: Query<&mut Node, With<PauseOverlay>>,
+) {
+    if keys.just_pressed(PAUSE_KEY) {
+        game_time.paused = !game_time.paused;
+
+        for mut ui_element in &mut query {
+            ui_element.display = match game_time.paused {
+                true => Display::Flex,
+                false => Display::None,
+            }
+        }
+    }
+}
+
+fn setup_pause_ui(mut commands: Commands) {
+    commands.spawn((
+        PauseOverlay,
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            display: Display::None,
+            ..default()
+        },
+        GlobalZIndex(-100),
+        PickingBehavior::IGNORE,
+        BackgroundColor(Color::linear_rgba(0.5, 0.5, 0.5, 0.3)),
+    ));
+
+    commands
+        .spawn((
+            PauseOverlay,
+            Node {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                display: Display::None,
+                flex_direction: FlexDirection::ColumnReverse,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            PickingBehavior::IGNORE,
+        ))
+        .with_child((Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(80.0),
+            flex_direction: FlexDirection::Row,
+            ..default()
+        },))
+        .with_child((
+            Text::new("Paused"),
+            TextFont::from_font_size(60.0),
+            TextColor::BLACK,
+            TextLayout::new_with_justify(JustifyText::Center).with_no_wrap(),
+        ));
 }
